5291ad1443f527e2dbbeffc8d4da8427a314a88f
michal-linuxmint <michal1315_1992@o2.pl>
2018-12-11 15:26:30 +0100

    add content to v_login and view_style files

---
+++ CMS/cms_login/views/v_login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>CMS Login</title>
+    <meta charset="UTF-8">
+    <!--suppress HtmlUnknownTarget -->
+    <link rel="stylesheet" type="text/css" href="views/view_style.css" />
+
+</head>
+<body>
+
+<h1>Logowanie</h1>
+<p>Akapit</p>
+
+
+</body>
+
+
+
+</html>
