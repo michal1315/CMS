39373feab589382a4483cd347097cfc979b8a72e
michal-linuxmint <michal1315_1992@o2.pl>
2018-12-16 19:43:46 +0100

    add more code which test alert fuction and check is styling capability

--- CMS/cms_login/login.php
+++ CMS/cms_login/login.php
@@ -6,7 +6,9 @@ include("models/m_template.php");
 
 $Template = new Template();
 $Template->setAlertTypes(array('success', 'warning', 'error'));
-$Template->setAlert("Powodzenie",'error');
+$Template->setAlert("Powodzenie",'success');
+$Template->setAlert("Uwaga",'warning');
+$Template->setAlert("Błąd",'error');
 
 $Template->setData("nazwa","wartość12");
 $Template->load("views/v_login.php");
