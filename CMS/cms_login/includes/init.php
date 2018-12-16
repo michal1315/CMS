db29f9db4f6908c284f722be56fe92e796cc2a21
michal-linuxmint <michal1315_1992@o2.pl>
2018-12-16 20:20:17 +0100

    add login validate function

---
+++ CMS/cms_login/includes/init.php
@@ -0,0 +1,9 @@
+<?php
+include ("models/m_template.php");
+include ("models/m_auth.php");
+
+$Template = new Template();
+$Template->setAlertTypes(array('success', 'warning', 'error'));
+$Auth = new Auth();
+
+session_start();
