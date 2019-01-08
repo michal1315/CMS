1c7527979ea2b798b9b848d8b42aafef11e9669e
michal-linuxmint <michal1315_1992@o2.pl>
2019-01-08 19:13:34 +0100

    add login control code

--- CMS/cms_login/login.php
+++ CMS/cms_login/login.php
@@ -1,15 +1,33 @@
 <?php
 
-include("models/m_template.php");
+include("includes/database.php");
+include("includes/init.php");
+
+
+if(isset($_POST['submit'])) {
+
+    $Template->setData('input_user', $_POST['username']);
+    $Template->setData('input_pass', $_POST['password']);
+
+
+    if ($_POST['username'] == '' || $_POST['password'] == '') {
+        $Template->setAlert('Uzupełnij wymagane pola', 'error');
+        $Template->load("views/v_login.php");
+    } else if ($Auth->validateLogin($Template->getData('input_user'), $Template->getData('input_pass') == FALSE)) {
+        $Template->setAlert('Nieprawidłowy login lub hasło', 'error');
+        $Template->load("views/v_login.php");
+    } else {
+        $_SESSION['username'] = $Template->getData('input_user');
+        $_SESSION['loggedin'] = TRUE;
+        $Template->setAlert('Witaj ' . $Template->getData('input_user'));
+        $Template->redirect('users.php');
+    }}
+
+    else{
+
+        $Template->load('views/v_login.php');
+    }
 
 
 
-$Template = new Template();
-$Template->setAlertTypes(array('success', 'warning', 'error'));
-$Template->setAlert("Powodzenie",'success');
-$Template->setAlert("Uwaga",'warning');
-$Template->setAlert("Błąd",'error');
-
-$Template->setData("nazwa","wartość12");
-$Template->load("views/v_login.php");
 
